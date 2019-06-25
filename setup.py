9e3a5e57aba6c944c54db3bfcfde621f8b4fa50e
Tom Ritchford <tom@swirly.com>
2019-06-25 18:30:48 +0200

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+import setuptools
+
+VERSION = '0.9.0'
+print('!!', [f for f in os.listdir('.') if f.startswith('git-')])
+
+setuptools.setup(
+    name='gitz',
+    version=VERSION,
+    author='Tom Ritchford',
+    author_email='tom@swirly.com',
+    url='https://github.com/rec/gitz',
+    description='🗜 gitz - tiny useful git commands, some dangerous 🗜',
+    long_description=open('README.rst').read(),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities',
+    ],
+    keywords=['git'],
+    scripts=[f for f in os.listdir('.') if f.startswith('git-')],
+)
