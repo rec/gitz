afe28037a12689f336e0dfed7b661e38990e812e
Tom Ritchford <tom@swirly.com>
2019-07-14 16:42:59 +0200

    Merge git_program.py into program.py

--- _gitz/program.py
+++ _gitz/program.py
@@ -1,6 +1,15 @@
+from . import git
+from . import util
+from .env import ENV
 from pathlib import Path
 import argparse
 import sys
+
+_ERROR_CHANGES_OVERWRITTEN = 'Your local changes would be overwritten'
+_ERROR_NOT_GIT_REPOSITORY = (
+    'fatal: not a git repository (or any of the parent directories): .git'
+)
+_ERROR_PROTECTED_BRANCHES = 'The branches %s are protected'
 
 
 class Program:
@@ -42,3 +51,20 @@ class Program:
             print(self.usage.rstrip())
             print(self.help.rstrip())
             return True
+
+    def require_git(self):
+        if not util.find_git_root():
+            self.error(_ERROR_NOT_GIT_REPOSITORY)
+            self.exit()
+
+    def require_clean_workspace(self):
+        self.require_git()
+        if git.is_workspace_dirty():
+            self.error(_ERROR_CHANGES_OVERWRITTEN)
+            self.exit()
+
+    def require_unprotected_branches(self, *branches):
+        pb = ENV.protected_branches()
+        if set(pb.split(':')).intersection(branches):
+            self.error(_ERROR_PROTECTED_BRANCHES % pb)
+            self.exit()
