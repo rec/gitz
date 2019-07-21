f6763d6a1ddf9a919d516fe03a9170a1feb89764
Tom Ritchford <tom@swirly.com>
2019-07-21 16:04:58 +0200

    Prepend exceptions with command line in Util.run

--- gitz/util.py
+++ gitz/util.py
@@ -4,17 +4,26 @@ import shlex
 import subprocess
 
 _SUBPROCESS_KWDS = {'encoding': 'utf-8', 'shell': True}
+_EXCEPTION_MSG = 'Encountered an exception while executing'
 
 
 def run(*cmd, use_shlex=False, verbose=False, **kwds):
     if verbose:
         print('$', *cmd)
     kwds = dict(_SUBPROCESS_KWDS, **kwds)
+    cmd_str = ' '.join(cmd)
+
     if kwds.get('shell'):
         if use_shlex:
             cmd = (shlex.quote(c) for c in cmd)
-        cmd = ' '.join(cmd)
-    lines = subprocess.check_output(cmd, **kwds).splitlines()
+        cmd = cmd_str
+
+    try:
+        lines = subprocess.check_output(cmd, **kwds).splitlines()
+    except Exception as e:
+        e.args = (_EXCEPTION_MSG, cmd_str) + e.args
+        raise
+
     if verbose:
         print(*lines, sep='')
     return lines
