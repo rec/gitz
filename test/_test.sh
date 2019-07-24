5bdb72aa82076d4c9b90505a67885b1f69e3e622
Tom Ritchford <tom@swirly.com>
2019-07-24 18:35:21 +0200

    Blacken Python before flake8 in test/_test.sh

--- test/_test.sh
+++ test/_test.sh
@@ -6,9 +6,9 @@ FILES=`grep -l python3 git-* | xargs echo`
 
 echo "Testing files $FILES"
 
+black -l 79 -S *.py test/*.py $FILES
 flake8
 flake8 $FILES
-black -l 79 -S *.py test/*.py $FILES
 export PATH=`pwd`:$PATH
 pytest
 
