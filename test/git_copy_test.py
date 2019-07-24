24a432d4abd66ba66cc5b6ee766063de32a90a30
Tom Ritchford <tom@swirly.com>
2019-07-24 21:01:06 +0200

    Add git_copy_test.py (Fix #17)

---
+++ test/git_copy_test.py
@@ -0,0 +1,14 @@
+from . import repo
+from gitz import git_functions
+from gitz.git import GIT
+import unittest
+
+
+class GitCopyTest(unittest.TestCase):
+    @repo.method
+    def test_simple(self):
+        repo.make_commit('1')
+        GIT.fresh('one')
+        GIT.copy('two')
+        expected = {'origin': ['master', 'one', 'two'], 'upstream': ['master']}
+        self.assertEqual(git_functions.all_branches(), expected)
