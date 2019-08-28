c4eae5e4a066c25e99f95d99ccc21699f020b9c0
Tom Ritchford <tom@swirly.com>
2019-08-28 12:10:59 +0200

    First version of individual command help

--- gitz/helper/command.py
+++ gitz/helper/command.py
@@ -7,41 +7,54 @@ def main(commands):
     for command, help in commands.items():
         help_file = (_HELP_DIRECTORY / command).with_suffix('.rst')
         with open(help_file, 'w') as fp:
-            _write(command, help, lambda *a: print(*a, file=fp))
-        return
+            Writer(command, help, fp).write()
 
 
-def _write(command, help, _print):
-    def header(line, underline='='):
-        _print(line)
-        _print(underline * len(line))
+class Writer:
+    def __init__(self, command, help, fp):
+        self.command = command
+        self.help = help
+        self.fp = fp
 
-    def print_lines(lines, *args):
-        for i, line in enumerate(lines):
-            if not i:
-                _print()
-            _print(*args, line)
+    def write(self):
+        for field, lines in self.help.items():
+            if field.startswith('git-'):
+                field = 'TITLE'
+            elif field.startswith('---'):
+                field = 'FLAGS'
+            method = getattr(self, '_' + field.lower(), None)
+            if method:
+                method(lines)
+            else:
+                self._default(field, lines)
 
-    for field, lines in help.items():
-        if field.startswith('git-'):
-            header('``%s``: %s' % (field, lines[0]), '-')
-            print_lines(lines[1:], '   ')
+    def _title(self, lines):
+        self._header('``%s``: %s' % (self.command, lines[0]), '-')
+        self._print_lines(lines[1:])
 
-        elif field.startswith('---'):
-            _print()
-            header('FLAGS')
-            print_lines(lines, '   ')
+    def _command(self, lines):
+        pass
 
-        elif field == 'COMMAND':
-            continue
+    def _usage(self, lines):
+        self._print()
+        self._header('USAGE')
+        self._print('.. code-block:: bash')
+        self._print_lines(lines)
 
-        elif field == 'USAGE':
-            _print()
-            header(field)
-            _print('.. code-block:: bash')
-            print_lines(lines, '   ')
+    def _default(self, field, lines):
+        self._print()
+        self._header(field)
+        self._print_lines(lines)
 
-        else:
-            _print()
-            header(field)
-            print_lines(lines, '   ')
+    def _print(self, *args):
+        print(*args, file=self.fp)
+
+    def _header(self, line, underline='='):
+        self._print(line)
+        self._print(underline * len(line))
+
+    def _print_lines(self, lines):
+        if lines:
+            self._print()
+        for line in lines:
+            self._print('   ', line)
