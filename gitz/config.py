3788318f8abeebef4c6863bceb38e9c0d3e07d91
Tom Ritchford <tom@swirly.com>
2019-09-01 13:42:09 +0200

    New version v0.9.8

--- gitz/config.py
+++ gitz/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-VERSION = '0.9.7'
+VERSION = '0.9.8'
 ROOT_DIR = Path(__file__).absolute().parents[1]
 COMMANDS = sorted(
     f.name for f in ROOT_DIR.iterdir() if f.name.startswith('git-')
