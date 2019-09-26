c708778a434407fed44d6d6df4ce70ecb72c7408
Tom Ritchford <tom@swirly.com>
2019-09-26 16:13:25 +0200

    Handle missing branches in git-delete (fix #117)

--- gitz/delete.py
+++ gitz/delete.py
@@ -4,10 +4,15 @@ from .program import git
 
 
 def delete(branches):
-    """Delete locally and upsteram"""
+    """Delete locally and upstream"""
     if not branches:
         PROGRAM.message('No branches specified')
         return
+
+    try:
+        origin = git_functions.upstream_branch()[0]
+    except Exception:
+        origin = ''
 
     deduped, dupes = [], []
     for b in branches:
@@ -20,8 +25,18 @@ def delete(branches):
 
     existing_branches = set(git_functions.branches())
     unknown = set(branches).difference(existing_branches)
+
+    remote_branches = git_functions.remote_branches()
     if unknown:
-        raise ValueError('Unknown', *sorted(unknown))
+        remotes = remote_branches.get(origin, [])
+        missing = unknown.difference(remotes)
+
+        if missing:
+            brs = 'branch' if len(missing) == 1 else 'branches'
+            miss = ', '.join(sorted(missing))
+            PROGRAM.error('Non-existent', brs, miss)
+    else:
+        missing = set()
 
     remaining_branches = existing_branches.difference(branches)
     if not remaining_branches:
@@ -30,9 +45,17 @@ def delete(branches):
     if git_functions.branch_name() in branches:
         git.checkout(sorted(remaining_branches)[0])
 
+    remotes_deleted = []
     for b in branches:
-        upstream = git_functions.upstream_branch(b)[0]
-        git.push(upstream, '--delete', b)
+        try:
+            upstream = git_functions.upstream_branch(b)[0]
+        except Exception:
+            upstream = origin
+        if upstream and b in remote_branches[upstream]:
+            git.push(upstream, '--delete', b)
+            remotes_deleted.append('%s/%s' % (upstream, b))
 
-    git.branch('-D', *branches)
-    return branches
+    local_branches = [b for b in branches if b not in unknown]
+    if local_branches:
+        git.branch('-D', *local_branches)
+    return local_branches + remotes_deleted
