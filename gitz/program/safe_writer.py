870c35c6815ffa9f5c62d913773cc30f2da0ff2e
Tom Ritchford <tom@swirly.com>
2019-11-04 15:23:16 +0100

    Now safe_writer can create parent directories

--- gitz/program/safe_writer.py
+++ gitz/program/safe_writer.py
@@ -1,20 +1,22 @@
 import contextlib
-import os
+from pathlib import Path
 
 SUFFIX = '.tmp'
 
 
 @contextlib.contextmanager
-def safe_writer(filename, suffix=SUFFIX):
-    tempfile = str(filename) + suffix
-    with open(tempfile, 'w') as fp:
+def safe_writer(filename, suffix=SUFFIX, create_parents=True):
+    tempfile = Path(str(filename) + suffix)
+    if create_parents:
+        tempfile.parent.mkdir(parents=True, exist_ok=True)
+    with tempfile.open('w') as fp:
         try:
             yield fp
         except Exception:
             try:
-                os.remove(tempfile)
+                tempfile.remove()
             except Exception:
                 pass
             raise
 
-    os.rename(tempfile, filename)
+    tempfile.rename(filename)
