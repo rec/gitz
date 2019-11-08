bafca5c161523898aac9ed945b9281f3308e1b92
Tom Ritchford <tom@swirly.com>
2019-11-08 12:52:40 +0100

    Enable tests for python 3.5
    Modify .travis.yml

--- test/_test.sh
+++ test/_test.sh
@@ -2,14 +2,11 @@
 
 set -Eeo pipefail
 
-python -m gitz_doc.get_command_summaries > gitz/program/summaries.py
-
 FILES=`grep -l python3 git-* | xargs echo`
 
 echo "Testing files $FILES"
 
-black -l 79 -S *.py test/*.py gitz/*.py gitz_doc/*.py $FILES
 flake8
 flake8 $FILES
 export PATH=`pwd`:$PATH
-pytest $@
+python3.5 -m unittest discover -p \*_test.py
