87f83e72c32d0a37915b0ecc7128632e6ea027c6
Tom Ritchford <tom@swirly.com>
2019-11-17 19:00:43 +0100

    Inline some code from termtosvg

--- gitz_doc/cast/render.py
+++ gitz_doc/cast/render.py
@@ -1,18 +1,14 @@
+from termtosvg import asciicast
+from termtosvg import anim
 from termtosvg import config
-from termtosvg import main
+from termtosvg import term
 
 TEMPLATE = 'base16_default_dark'
-RENDER = {
-    'still': False,
-    'min_frame_duration': 1,
-    'max_frame_duration': None,
-    'loop_delay': main.DEFAULT_LOOP_DELAY,
-}
 
 
 def render(cast_file, svg_file):
-    main.render_subcommand(
-        template=config.default_templates()[TEMPLATE],
-        cast_filename=str(cast_file),
-        output_path=str(svg_file),
-        **RENDER)
+    template = config.default_templates()[TEMPLATE]
+
+    asciicast_records = asciicast.read_records(str(cast_file))
+    geometry, frames = term.timed_frames(asciicast_records)
+    anim.render_animation(frames, geometry, str(svg_file), template)
