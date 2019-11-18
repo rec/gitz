6bdbf40247b4dac0d04f7a5f4539d863940b37f0
Tom Ritchford <tom@swirly.com>
2019-11-18 16:48:09 +0100

    Use pytest in test/_test_dev.sh

--- test/_test_dev.sh
+++ test/_test_dev.sh
@@ -6,4 +6,8 @@ python -m gitz_doc.get_command_summaries > gitz/program/summaries.py
 
 FILES=`grep -l python3 git-* | xargs echo`
 black -l 79 -S *.py test/*.py gitz/*.py gitz_doc/*.py $FILES
-source test/_test.sh
+
+flake8
+flake8 $FILES
+export PATH=`pwd`:$PATH
+pytest $@
