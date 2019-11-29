3c5ef70a5dc05b480a78dc1f6a51fc45dbd43c41
Tom Ritchford <tom@swirly.com>
2019-11-29 16:13:11 +0100

    Sample scripts for generated movies

---
+++ gitz_doc/cast/scripts/git-adjust.sh
@@ -0,0 +1,21 @@
+# git-adjust: like git commit --amend
+# but works for any commit, not just the most recent
+
+touch main.py read.py read_test.py write.py write_test.py
+
+git add read.py
+git commit -m Read
+
+git add *write.py
+git commit -m Write
+
+git add main.py
+git commit -m Main
+
+git st
+# oops!
+
+git log --oneline
+
+git add test_read.py
+git adjust HEAD~~ test_read.py
