d0958e55e66c0d355c889dafa42c5fe768f957a7
Tom Ritchford <tom@swirly.com>
2019-12-09 13:50:20 +0100

    Add --head flag to git-new

--- test/git_new_test.py
+++ test/git_new_test.py
@@ -19,3 +19,29 @@ class GitNewTest(unittest.TestCase):
         actual = GIT.log('--oneline', 'origin/two')
         expected = ['aff4d90 2', 'c0d1dbb 0']
         self.assertEqual(actual, expected)
+
+    @repo.test
+    def test_use_head(self):
+        GIT.new('one', '-vu')
+        repo.make_commit('1')
+        GIT.push()
+        actual = GIT.log('--oneline', 'origin/one')
+        expected = ['a03c0f8 1', 'c0d1dbb 0']
+        self.assertEqual(actual, expected)
+
+        GIT.new('two', '-vu')
+        repo.make_commit('2')
+        GIT.push()
+        actual = GIT.log('--oneline', 'origin/two')
+        expected = ['043df1f 2', 'a03c0f8 1', 'c0d1dbb 0']
+        self.assertEqual(actual, expected)
+
+    @repo.test
+    def test_use_head_unstaged(self):
+        repo.make_commit('1')
+        repo.write_file('1', '2')
+        GIT.new('one', '-vu')
+        self.assertEqual(open('1').read(), '2')
+        actual = GIT.log('--oneline', 'origin/one')
+        expected = ['a03c0f8 1', 'c0d1dbb 0']
+        self.assertEqual(actual, expected)
