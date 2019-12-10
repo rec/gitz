f1b6b8d5e1103b176dad37c57ad54024c112ac11
Tom Ritchford <tom@swirly.com>
2019-12-10 12:05:09 +0100

    Simplify cast/needs_update.py

--- gitz_doc/cast/needs_update.py
+++ gitz_doc/cast/needs_update.py
@@ -4,30 +4,33 @@ ROOT = Path(__file__).parent
 
 
 class Updater:
+    # These need to be overridden in the derived class
+    _target = _source = _create = _existing = None
+
     @classmethod
     def update(cls, commands):
         results = []
         for c in commands:
-            symbol, result = cls._update(cls._target(c), cls._source(c))
-            if result:
-                results.append((c, result))
+            target = cls._target(c)
+            source = cls._source(c)
+            if not source.exists():
+                print('?', target)
+                continue
+
+            if target.exists():
+                new = True
+            else:
+                src = tuple(f for f in ROOT.iterdir() if f.suffix == '.py')
+                newest = max(f.stat().st_mtime for f in src + (source,))
+                new = target.stat().st_mtime < newest
+
+            if new:
+                symbol = '+'
+                cast = cls._create(target, source)
+            else:
+                symbol = '.'
+                cast = cls._existing(target, source)
+
+            results.append((symbol, cast))
 
         return results
-
-    @classmethod
-    def _update(cls, target, source):
-        if not source.exists():
-            return '?', None
-
-        new = cls._needs_update(target, source)
-        symbol = '+' if new else '.'
-        method = cls._create if new else cls._existing
-        return symbol, method(target, source)
-
-    @classmethod
-    def _needs_update(cls, target, source):
-        if not target.exists():
-            return True
-        src = tuple(f for f in ROOT.iterdir() if f.suffix == '.py')
-        newest = max(f.stat().st_mtime for f in src + (source,))
-        return target.stat().st_mtime < newest
