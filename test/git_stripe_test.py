9705c5855001036dcc219223df7ca985057e6655
Tom Ritchford <tom@swirly.com>
2019-12-12 17:17:48 +0100

    Better argument checking for git-stripe

--- test/git_stripe_test.py
+++ test/git_stripe_test.py
@@ -7,28 +7,17 @@ import unittest
 class GitStripeTest(unittest.TestCase):
     @repo.test
     def test_stripe(self):
-        repo.make_commit('1')
-        repo.make_commit('2')
-        repo.make_commit('3')
-        repo.make_commit('4')
-
-        GIT.push('-u', 'origin', 'master')
-        GIT.stripe('3', '-v')
-
-        actual = functions.branches('-r')
-        expected = [
-            'origin/_gitz_stripe_0',
-            'origin/_gitz_stripe_1',
-            'origin/_gitz_stripe_2',
-            'origin/master',
-            'upstream/master',
-        ]
-        self.assertEqual(actual, expected)
-
+        self._setup()
         GIT.stripe('3', '-d', '-v')
         actual = functions.branches('-r')
         expected = ['origin/master', 'upstream/master']
         self.assertEqual(actual, expected)
+
+    @repo.test
+    def test_error(self):
+        self._setup()
+        with self.assertRaises(ValueError):
+            GIT.stripe('3', '-D', '-v')
 
     @repo.test
     def test_range_setting(self):
@@ -40,6 +29,25 @@ class GitStripeTest(unittest.TestCase):
 
         GIT.push('-u', 'origin', 'master')
         GIT.stripe(two, four, '-v')
+
+        actual = functions.branches('-r')
+        expected = [
+            'origin/_gitz_stripe_0',
+            'origin/_gitz_stripe_1',
+            'origin/_gitz_stripe_2',
+            'origin/master',
+            'upstream/master',
+        ]
+        self.assertEqual(actual, expected)
+
+    def _setup(self):
+        repo.make_commit('1')
+        repo.make_commit('2')
+        repo.make_commit('3')
+        repo.make_commit('4')
+
+        GIT.push('-u', 'origin', 'master')
+        GIT.stripe('3', '-v')
 
         actual = functions.branches('-r')
         expected = [
