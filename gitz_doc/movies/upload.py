a92e46c08f7e31a5aad39ec502812a64c6fb6694
Tom Ritchford <tom@swirly.com>
2019-12-27 11:06:41 +0100

    Open the webpage for an uploaded movie

--- gitz_doc/movies/upload.py
+++ gitz_doc/movies/upload.py
@@ -3,6 +3,7 @@ from .constants import ALL_COMMANDS_JSON
 from gitz.program import run_proc
 import hashlib
 import json
+import webbrowser
 
 
 def all_movie_url():
@@ -18,15 +19,12 @@ def all_movie_url():
     lines = run_proc.run_proc(COMMAND)
     url = next(i for i in lines if 'https://' in i).strip()
     ALL_COMMANDS_JSON.write_text(json.dumps([sha, url]))
+    webbrowser.open(url, new=0, autoraise=True)
     print(MESSAGE.format(url=url))
 
 
 COMMAND = 'asciinema', 'upload', str(ALL_COMMANDS_CAST)
 MESSAGE = """
-New upload created!
-
-Now you need to make the upload public and change its name to
-"The gitz movie".  Go to:
-
-   {url}
+New upload {url}!
+Make it public and change its name to "The gitz movie".
 """
