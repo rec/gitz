67e45e70b7046ac21658826d9c866be797f59419
Tom Ritchford <tom@swirly.com>
2020-01-06 18:18:50 +0100

    git-adjust gets -a/-A flags (fix #198)

--- test/git_adjust_test.py
+++ test/git_adjust_test.py
@@ -51,6 +51,28 @@ class GitAdjustTest(unittest.TestCase):
         self.assertEqual(['5'], _files_in_commit('HEAD~'))
         self.assertEqual(['4', '6'], _files_in_commit('HEAD~~'))
 
+    @repo.test
+    def test_all_tracked(self):
+        repo.make_seven_commits(self)
+        repo.write_file('6', 'DELTA')
+        GIT.adjust('-a', '-v')
+        actual = GIT.log('--oneline')[:4]
+        expected = ['067af20 7', 'e1e931a 6', '8a4a4e2 5', 'a7c7e8f 4']
+        self.assertEqual(expected, actual)
+
+        self.assertEqual(['6', '7'], _files_in_commit())
+
+    @repo.test
+    def test_all_files(self):
+        repo.make_seven_commits(self)
+        GIT.reset('HEAD~')
+        GIT.adjust('-A', '-v')
+        actual = GIT.log('--oneline')[:4]
+        expected = ['84e8ee1 6', '8a4a4e2 5', 'a7c7e8f 4', '9ab30c5 3']
+        self.assertEqual(expected, actual)
+
+        self.assertEqual(['6', '7'], _files_in_commit())
+
 
 def _files_in_commit(commit='HEAD'):
     return GIT.diff_tree('--no-commit-id', '--name-only', '-r', commit)
