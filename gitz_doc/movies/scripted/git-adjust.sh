aa2c9bb5c80c572edc3ccc09e949bb2e4eb2ade8
Tom Ritchford <tom@swirly.com>
2020-01-07 15:10:20 +0100

    Temporarily speed up films for debugging

--- gitz_doc/movies/scripted/git-adjust.sh
+++ gitz_doc/movies/scripted/git-adjust.sh
@@ -3,10 +3,8 @@
 
 touch main.py read.py read_test.py write.py write_test.py
 
-git add read.py && git commit -m Read
-
+git add read.py && git commit -m Read  # Missed a file!
 git add write*.py && git commit -m Write
-
 git add main.py && git commit -m Main
 
 git st
@@ -14,3 +12,4 @@ git st
 
 git log --oneline
 git adjust -A HEAD~~
+git show HEAD~~
