229a2defa3af45e8a0a2fa796f072904d5898f99
Tom Ritchford <tom@swirly.com>
2020-01-19 14:44:53 +0100

    Improve git-update's movie

--- gitz_doc/movies/scripted/git-update.sh
+++ gitz_doc/movies/scripted/git-update.sh
@@ -1,13 +1,13 @@
 # git-update - rebase and force-push all working branches from upstream
 
 git new one
-touch one.txt && git add one.txt && git commit -m one && git push
+touch one.txt && git add one.txt && git commit -m one && git push -q
 
 git new two
-touch two.txt && git add two.txt && git commit -m two && git push
+touch two.txt && git add two.txt && git commit -m two && git push -q
 
 git checkout master
-touch zero.txt && git add zero.txt && git commit -m zero && git push
+touch zero.txt && git add zero.txt && git commit -m zero && git push -q
 
 git update
 git for-each - git log --oneline
