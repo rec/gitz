cd2b750eddf65b97dcbd325a0c0c0d42e030cddb
Tom Ritchford <tom@swirly.com>
2020-10-07 21:07:02 +0200

    Enable Python 3.9

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: MIT License',
             'Topic :: Utilities',
