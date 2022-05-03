69233fbbbc51644daeb2f61c369a52f7a86652d0
Tom Ritchford <tom@swirly.com>
2022-05-03 15:27:27 +0200

    Version v1.1.2

--- gitz/config.py
+++ gitz/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import sys
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 HOME_PAGE = 'https://github.com/rec/gitz/blob/master/README.rst'
 
 # In development and when running tests, LIBRARY_DIRECTORY and
