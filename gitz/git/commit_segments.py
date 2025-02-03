08287b4f5ca734330aa94e17d00286f0f38f39bb
Tom Ritchford <tom@swirly.com>
2025-02-03 18:38:30 +0100

    Add segments and test

---
+++ gitz/git/commit_segments.py
@@ -0,0 +1,25 @@
+from typing import Iterator
+
+
+def segments(s: str) -> Iterator[str]:
+    stack = ""
+    for i in s:
+        if i not in "+-" and stack and stack[-1] not in "+-":
+            yield stack
+            stack = ""
+        stack += i
+    if stack:
+        yield stack
+
+
+def segment_to_commits(segment: str) -> str:
+    "a+b-c+d"
+    assert len(segment) % 2 == 1, segment
+    segment += "-"
+    name, sign = zip(*(segment[i:i + 2] for i in range(0, len(segment), 2)))
+    return "".join(name), sign.index("-")
+
+
+def segments_to_commits(s):
+    for seg in segments(s):
+        yield segment_to_commits(seg)
---
+++ test/git_segments_test.py
@@ -0,0 +1,32 @@
+from gitz.git.commit_segments import segments_to_commits
+import unittest
+
+
+class GitSegmentsTest(unittest.TestCase):
+    def test_simple(self):
+        actual = list(segments_to_commits(""))
+        expected = []
+        self.assertEqual(actual, expected)
+
+        actual = list(segments_to_commits("a"))
+        expected = [('a', 0)]
+        self.assertEqual(actual, expected)
+
+        actual = list(segments_to_commits("ab"))
+        expected = [('a', 0), ('b', 0)]
+        self.assertEqual(actual, expected)
+
+        actual = list(segments_to_commits("a+b"))
+        expected = [('ab', 1)]
+        self.assertEqual(actual, expected)
+
+        actual = list(segments_to_commits("a-b"))
+        expected = [('ab', 0)]
+        self.assertEqual(actual, expected)
+
+    def test_complex(self):
+        actual = list(segments_to_commits("abc+de-fgh+i+j-k"))
+        expected = [
+            ('a', 0),  ('b', 0),  ('cd', 1),  ('ef', 0),  ('g', 0),  ('hijk', 2)
+        ]
+        self.assertEqual(actual, expected)
